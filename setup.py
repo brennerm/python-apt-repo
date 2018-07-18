4025e1a155b1e0d1268d118f4241aa759fe833b8
brennerm <xamrennerb@gmail.com>
2018-07-18 20:30:46 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='apt-repo',
+    version='0.2',
+    packages=['apt_repo'],
+    url='https://github.com/brennerm/python-apt-repo',
+    license='MIT',
+    author='brennerm',
+    author_email='xamrennerb@gmail.com',
+    description='Python library to query APT repositories'
+)
