833482896038d57a3ac231ef030a963bffea1c5c
Dmitry Tyzhnenko <t.dmitry@gmail.com>
2020-08-16 23:44:38 +0300

    Add additinal fields to BinaryPackage
    Add properties for those package attributes:
        Maintainer
        Original-Maintainer
        Architecture
        Multi-Arch
        Homepage
        Origin
        Priority
        Section
        Depends
        Replaces
        Conflicts
        Breaks
        Suggests
        Recommends
        Size
        Installed-Size
        MD5Sum
        SHA1
        SHA256
        Description
        Description-md5
        Built-Using
        Source
        Task
        Supported

--- apt_repo/__init__.py
+++ apt_repo/__init__.py
@@ -164,6 +164,216 @@ class BinaryPackage:
     def filename(self):
         return _get_value(self.__content, 'Filename')
 
+    @property
+    def maintainer(self):
+        try:
+            return _get_value(self.__content, 'Maintainer')
+        except KeyError:
+            return None
+
+    @property
+    def original_maintainer(self):
+        try:
+            return _get_value(self.__content, 'Original-Maintainer')
+        except KeyError:
+            return None
+
+    @property
+    def architecture(self):
+        try:
+            return _get_value(self.__content, 'Architecture')
+        except KeyError:
+            return None
+
+    @property
+    def multi_arch(self):
+        try:
+            return _get_value(self.__content, 'Multi-Arch')
+        except KeyError:
+            return None
+
+    @property
+    def homepage(self):
+        try:
+            return _get_value(self.__content, 'Homepage')
+        except KeyError:
+            return None
+
+    @property
+    def origin(self):
+        try:
+            return _get_value(self.__content, 'Origin')
+        except KeyError:
+            return None
+
+    @property
+    def priority(self):
+        try:
+            return _get_value(self.__content, 'Priority')
+        except KeyError:
+            return None
+
+    @property
+    def section(self):
+        try:
+            return _get_value(self.__content, 'Section')
+        except KeyError:
+            return None
+
+    @property
+    def depends(self):
+        try:
+            field = _get_value(self.__content, 'Depends')
+        except KeyError:
+            return None
+
+        if field:
+            depends = [d.strip() for d in field.split(',')]
+        else:
+            depends = []
+        return depends
+
+    @property
+    def replaces(self):
+        try:
+            field = _get_value(self.__content, 'Replaces')
+        except KeyError:
+            return None
+
+        if field:
+            replaces = [d.strip() for d in field.split(',')]
+        else:
+            replaces = []
+        return replaces
+
+    @property
+    def breaks(self):
+        try:
+            field = _get_value(self.__content, 'Breaks')
+        except KeyError:
+            return None
+
+        if field:
+            breaks = [d.strip() for d in field.split(',')]
+        else:
+            breaks = []
+        return breaks
+
+    @property
+    def recommends(self):
+        try:
+            field = _get_value(self.__content, 'Recommends')
+        except KeyError:
+            return None
+
+        if field:
+            recommends = [d.strip() for d in field.split(',')]
+        else:
+            recommends = []
+        return recommends
+
+    @property
+    def suggests(self):
+        try:
+            field = _get_value(self.__content, 'Suggests')
+        except KeyError:
+            return None
+
+        if field:
+            suggests = [d.strip() for d in field.split(',')]
+        else:
+            suggests = []
+        return suggests
+
+    @property
+    def conflicts(self):
+        try:
+            field = _get_value(self.__content, 'Conflicts')
+        except KeyError:
+            return None
+
+        if field:
+            conflicts = [d.strip() for d in field.split(',')]
+        else:
+            conflicts = []
+        return conflicts
+
+    @property
+    def installed_size(self):
+        try:
+            return _get_value(self.__content, 'Installed-Size')
+        except KeyError:
+            return None
+
+    @property
+    def size(self):
+        try:
+            return _get_value(self.__content, 'Size')
+        except KeyError:
+            return None
+
+    @property
+    def md5(self):
+        try:
+            return _get_value(self.__content, 'MD5Sum')
+        except KeyError:
+            return None
+
+    @property
+    def sha1(self):
+        try:
+            return _get_value(self.__content, 'SHA1')
+        except KeyError:
+            return None
+
+    @property
+    def sha256(self):
+        try:
+            return _get_value(self.__content, 'SHA256')
+        except KeyError:
+            return None
+
+    @property
+    def description(self):
+        try:
+            return _get_value(self.__content, 'Description')
+        except KeyError:
+            return None
+
+    @property
+    def description_md5(self):
+        try:
+            return _get_value(self.__content, 'Description-md5')
+        except KeyError:
+            return None
+
+    @property
+    def built_using(self):
+        try:
+            return _get_value(self.__content, 'Built-Using')
+        except KeyError:
+            return None
+
+    @property
+    def source(self):
+        try:
+            return _get_value(self.__content, 'Source')
+        except KeyError:
+            return None
+
+    @property
+    def task(self):
+        try:
+            return _get_value(self.__content, 'Task')
+        except KeyError:
+            return None
+
+    @property
+    def supported(self):
+        try:
+            return _get_value(self.__content, 'Supported')
+        except KeyError:
+            return None
 
 class APTRepository:
     """
